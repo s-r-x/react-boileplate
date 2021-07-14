3b5c0d4f09dbc8acb8d272d14a20704f461c8701
s-r-x <strax1990@gmail.com>
2021-07-14 12:02:57 +0500

    rm progress-bar-webpack-plugin from the dev config

--- .internals/webpack.dev.js
+++ .internals/webpack.dev.js
@@ -2,7 +2,6 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const webpack = require("webpack");
 const devServer = require("./parts/devServer");
-const ProgressBarPlugin = require("progress-bar-webpack-plugin");
 const styleLoaders = require("./parts/styleLoaders");
 const { STYLE_REGEX } = require("./constants");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
